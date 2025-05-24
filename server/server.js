fc0f56d77d5302c9b90b8f727aa5a4e602cbf5a6
GanderM1 <mihailguskov00@gmail.com>
2025-05-25 00:33:32 +0300

    r

--- server/server.js
+++ server/server.js
@@ -447,31 +447,31 @@ app.post("/api/auth/register", async (req, res) => {
       return res.status(400).json({ error: "ФИ и пароль обязательны" });
     }
 
-    // Приведение ФИ к формату: каждое слово с заглавной
-    const normalizeFullName = (name) =>
-      name
-        .toLowerCase()
-        .trim()
-        .replace(/(?:^|\s|-)[а-яё]/g, (letter) => letter.toUpperCase());
+    // // Приведение ФИ к формату: каждое слово с заглавной
+    // const normalizeFullName = (name) =>
+    //   name
+    //     .toLowerCase()
+    //     .trim()
+    //     .replace(/(?:^|\s|-)[а-яё]/g, (letter) => letter.toUpperCase());
 
-    username = normalizeFullName(username);
+    // username = normalizeFullName(username);
 
-    // Валидация символов
-    const validUsernameRegex = /^[А-Яа-яЁё\- ]+$/;
-    const capitalLetters = username.match(/[А-ЯЁ]/g) || [];
+    // // Валидация символов
+    // const validUsernameRegex = /^[А-Яа-яЁё\- ]+$/;
+    // const capitalLetters = username.match(/[А-ЯЁ]/g) || [];
 
-    if (!validUsernameRegex.test(username)) {
-      return res.status(400).json({
-        error: "ФИ может содержать только кириллицу, пробелы и дефисы",
-      });
-    }
+    // if (!validUsernameRegex.test(username)) {
+    //   return res.status(400).json({
+    //     error: "ФИ может содержать только кириллицу, пробелы и дефисы",
+    //   });
+    // }
 
-    if (capitalLetters.length < 2) {
-      return res.status(400).json({
-        error:
-          "ФИ должно содержать как минимум две заглавные буквы (например, имя и фамилия)",
-      });
-    }
+    // if (capitalLetters.length < 2) {
+    //   return res.status(400).json({
+    //     error:
+    //       "ФИ должно содержать как минимум две заглавные буквы (например, имя и фамилия)",
+    //   });
+    // }
 
     // Проверка роли
     if (!["student", "teacher", "admin"].includes(role)) {
